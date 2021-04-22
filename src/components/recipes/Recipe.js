5a2b1cb741122f97acfa4e0e2593c78fa95b1878
moscuet <mostifizur.rahman@integrify.iogit config --global user.email mostifizur.rahman@integrify.io>
2021-04-22 14:06:27 +0300

    Recipes css modified

--- src/components/recipes/Recipe.js
+++ src/components/recipes/Recipe.js
@@ -5,7 +5,7 @@ export default function Recipe({recipe}) {
     return (
         <div className="recipe-box">
             <div className="image-div">
-                <img src={recipe.image[0]} alt='recipie pic'></img>
+                <img src={recipe.image[0]} alt='recipie pic'/>
             </div>
             <p>{recipe.name}</p>
         </div>
